ac92cdfb4fff0068d4849b5e41d60e2f3160d3b0
Savindu@Pasintha <64083148+savindu-pasintha@users.noreply.github.com>
2022-04-17 19:15:09 +0530

    Update DefaultSecurityConfigurations.java

--- spring-boot-full-course/src/main/java/com/savindupasintha/DefaultSecurityConfigurations.java
+++ spring-boot-full-course/src/main/java/com/savindupasintha/DefaultSecurityConfigurations.java
@@ -1,25 +1,51 @@
 package com.savindupasintha;
 /*
+
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 
 @SpringBootApplication
 @Configuration
 @EnableWebSecurity
 public class DefaultSecurityConfigurations extends WebSecurityConfigurerAdapter{
-	//1-default security login page disabled
+
 	protected void configure(HttpSecurity security) throws Exception
     {
-     security.httpBasic().disable();
+        //1-default security login page disabled
+        security.httpBasic().disable();
+
+         //2- csrf error fic- post request enable
+         security.cors().and().csrf().disable();
     }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        //2- csrf error fic- post request enable
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
 //https://medium.com/javarevisited/all-you-need-to-know-about-spring-security-basics-aea98c680d01
 //https://www.toptal.com/spring/spring-security-tutorial
 
     //custom username-password
+	/*
     @Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
@@ -27,6 +53,8 @@ public class DefaultSecurityConfigurations extends WebSecurityConfigurerAdapter{
         .password("{noop}password")
         .roles("USER");
 	}
+	 */
 
 }
+
 */
